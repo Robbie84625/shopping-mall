b00c38e5c8f66fd7febacee573a1772b9a8fda5e
Robbie84625 <loby84625@gmail.com>
2024-06-24 17:00:55 +0800

    商品功能 - 查詢商品列表(排序)

--- src/main/java/com/robbie/shoppingmall/dto/ProductQueryParams.java
+++ src/main/java/com/robbie/shoppingmall/dto/ProductQueryParams.java
@@ -1,13 +1,17 @@
 package com.robbie.shoppingmall.dto;
 
 import com.robbie.shoppingmall.constant.ProductCategory;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class ProductQueryParams {
     private String keyword;
     private ProductCategory productCategory;
-    private Integer page;/
+    private Integer page;
+    private String sort;
+    private String orderBy;
 }
